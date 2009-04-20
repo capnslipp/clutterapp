f192c236fc8315666fd1a561ef16e832ba3bfd23
Slippy Douglas <git@slippyd.com>
2009-04-20 03:03:41 +0000

    Fixed incorrect TagProp field name.
    git-svn-id: http://svn.6bitt.com/orgclut2/trunk@698 1f3b4c75-c14c-44bc-a5a0-28947426f03f

--- db/migrate/20090408061957_create_tag_props.rb
+++ db/migrate/20090408061957_create_tag_props.rb
@@ -1,7 +1,7 @@
 class CreateTagProps < ActiveRecord::Migration
   def self.up
     create_table :tag_props do |t|
-      t.string :text, :null => false
+      t.string :tag, :null => false
       
       t.timestamps
     end
