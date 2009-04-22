3097be4ff3e0c54f285d254c0da6e9fc34d505c5
Slippy Douglas <git@slippyd.com>
2009-04-22 07:19:49 +0000

    FINALLY dried up my views and got everything rendering with the help of a helper.
    git-svn-id: http://svn.6bitt.com/orgclut2/trunk@708 1f3b4c75-c14c-44bc-a5a0-28947426f03f

--- app/cells/node_cell.rb
+++ app/cells/node_cell.rb
@@ -10,29 +10,10 @@ class NodeCell < Cell::Base
   
   def show_item
     @node = @opts[:node]
-    @children = @node.children
+    @children = @node.children || []
     @prop = @node.prop
-        
-    #state = 'show_item'
     
-    ### DISCUSS: create Cell::View directly? maybe we can fix a gettext problem this way?
-    #view_class  = Class.new(Cell::View)
-    #action_view = view_class.new(@@view_paths, {}, @controller)
-    #action_view.cell = self
-    ### FIXME/DISCUSS: 
-    #action_view.template_format = :html # otherwise it's set to :js in AJAX context!
-    
-    # Make helpers and instance vars available
-    #include_helpers_in_class(view_class)
-    
-    #action_view.assigns = assigns_for_view
-    
-    
-    #template = find_family_view_for_state(state, action_view)
-    ### TODO: cache family_view for this cell_name/state in production mode,
-    ###   so we can save the call to possible_paths_for_state.
-    
-    #action_view.render(:file => template) 
+    controller.append_view_path 'views/nodes'
     
     nil
   end
--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -4,44 +4,20 @@ module NodesHelper
     ((cell ? cell[1] : '') + 'Node').underscore.to_sym
   end
   
-  def node_cell_with_content(content)
-    out = ''
-    out << <<-EOD
-<li class="main-item">
-	<div class="body">
-		<div class="manipulate buttons shadowable">
-			<div class="shadow"></div>
-			<a href="#" class="sym">&#10006;</a>
-			<a href="#" class="sym">&#9668;</a>
-			<a href="#" class="sym">&#9650;</a>
-			<a href="#" class="sym">&#9660;</a>
-			<a href="#" class="sym">&#9658;</a>
-			<a href="#" class="sym">&#9673;</a>
-		</div>
-		#{content}
-	</div>
-    EOD
-    
-    unless @node.children.empty?
-      out << '<ul class="main-list">'
-      @node.children.each do |child|
-        cell = Cell::Base.create_cell_for(@controller, cell_for_node(child), :node => child)
-        out << cell.render_state(:show_item)
-      end
-      out << '</ul>'
-    end
-    
-    out << <<-EOD
-	<div class="new buttons shadowable">
-		<div class="shadow"></div>
-		<a href="#"><span class="sym">&#10010;</span>text</a>
-		<a href="#"><span class="sym">&#10010;</span></span>check</a>
-		<a href="#"><span class="sym">&#10010;</span></span>note</a>
-		<a href="#"><span class="sym">&#10010;</span></span>priority</a>
-		<a href="#"><span class="sym">&#10010;</span></span>tag</a>
-		<a href="#"><span class="sym">&#10010;</span></span>time</a>
-	</div>
-</li>
-    EOD
+  def render_child_node(view, child_node)
+    view.render_cell(cell_for_node(child_node), :show_item, :node => child_node)
+  end
+  
+  def render_node_cell_layout(cell_view, &block)
+    cell_view.render(
+      :layout => '../views/nodes/item',
+      :object => cell_view.assigns['node'],
+      :locals => {
+        :render_child => proc {|child_node|
+          cell_view.render_cell(cell_for_node(child_node), :show_item, :node => child_node)
+        }
+      },
+      &block
+    )
   end
 end
