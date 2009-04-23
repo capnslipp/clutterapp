c10a50ebd472743741096950ac5df981a763e95b
Slippy Douglas <git@slippyd.com>
2009-04-23 09:09:46 +0000

    Removed unused code.
    git-svn-id: http://svn.6bitt.com/orgclut2/trunk@710 1f3b4c75-c14c-44bc-a5a0-28947426f03f

--- app/cells/node_cell.rb
+++ app/cells/node_cell.rb
@@ -2,18 +2,10 @@ class NodeCell < Cell::Base
   include NodesHelper
   helper_method NodesHelper.public_instance_methods
   
-  
-  #include ActionView::Helpers::CaptureHelper
-  #include ActionView::Helpers::TagHelper
-  #include ActionView::Helpers::TextHelper
-  
-  
   def show_item
     @node = @opts[:node]
     @children = @node.children || []
     @prop = @node.prop
-    
-    controller.append_view_path 'views/nodes'
     
     nil
   end
