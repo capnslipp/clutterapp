6314f6964de0c682f793143bda4ea0d100fa7d2c
Slippy Douglas <git@slippyd.com>
2009-09-22 16:50:19 -0700

    Added a test for the invalid condition as well.

--- test/unit/invite_test.rb
+++ test/unit/invite_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class InviteTest < ActiveSupport::TestCase
-  def test_should_be_valid
+  def test_new_invite_with_recipent_email_should_be_valid
     assert Invite.new(:recipient_email => 'test@example.com').valid?
+  end
+  
+  def test_new_invite_should_not_be_valid
+    assert !( Invite.new().valid? )
   end
 end
