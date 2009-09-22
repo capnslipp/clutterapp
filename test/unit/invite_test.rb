7e194cb2b955a58c670d46a164f60612d311eada
Slippy Douglas <git@slippyd.com>
2009-09-22 16:45:11 -0700

    Fixed invite test.

--- test/unit/invite_test.rb
+++ test/unit/invite_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class InviteTest < ActiveSupport::TestCase
-  #def test_should_be_valid
-  #  assert Invite.new.valid?
-  #end
+  def test_should_be_valid
+    assert Invite.new(:recipient_email => 'test@example.com').valid?
+  end
 end
