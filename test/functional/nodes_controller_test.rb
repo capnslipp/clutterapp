73e1493f13ce7bec85c5efcd56a74c4869cda686
Slippy Douglas <git@slippyd.com>
2009-09-24 12:32:04 -0700

    Commented out tests that were failing due to the action no longer being present.

--- test/functional/nodes_controller_test.rb
+++ test/functional/nodes_controller_test.rb
@@ -6,40 +6,44 @@ class NodesControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:nodes)
   end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
+  
+  # no direct "new" action
+  #test "should get new" do
+  #  get :new
+  #  assert_response :success
+  #end
+  
   test "should create node" do
     assert_difference('Node.count') do
       post :create, :node => { }
     end
-
+    
     assert_redirected_to node_path(assigns(:node))
   end
-
-  test "should show node" do
-    get :show, :id => nodes(:one).to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => nodes(:one).to_param
-    assert_response :success
-  end
-
-  test "should update node" do
-    put :update, :id => nodes(:one).to_param, :node => { }
-    assert_redirected_to node_path(assigns(:node))
-  end
-
+  
+  # no direct "update" action
+  #test "should show node" do
+  #  get :show, :id => nodes(:one).to_param
+  #  assert_response :success
+  #end
+  
+  # no direct "update" action
+  #test "should get edit" do
+  #  get :edit, :id => nodes(:one).to_param
+  #  assert_response :success
+  #end
+  
+  # no direct "update" action
+  #test "should update node" do
+  #  put :update, :id => nodes(:one).to_param, :node => { }
+  #  assert_redirected_to node_path(assigns(:node))
+  #end
+  
   test "should destroy node" do
     assert_difference('Node.count', -1) do
       delete :destroy, :id => nodes(:one).to_param
     end
-
+    
     assert_redirected_to nodes_path
   end
 end
