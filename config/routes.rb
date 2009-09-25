39f6c919e38236ebc1909710b4c9735f591b7e8e
Slippy Douglas <git@slippyd.com>
2009-09-25 10:56:38 -0700

    Fixed up routes to properly use default_routing. Rearranged and commented things a bit for clarity.

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller => 'home'
+# friendly user URLs
   
+  map.root :controller => 'home'
   
   map.login   '/sin',               :controller => 'sessions',  :action => 'create'
   map.logout  '/sout',              :controller => 'sessions',  :action => 'destroy'
+  map.resource :session, :as => 'sess'
+  
   map.register '/reg',              :controller => 'users',     :action => 'create'
-  #map.signup  '/sup',            :controller => 'users',     :action => 'new'
+  #map.signup  '/sup',               :controller => 'users',     :action => 'new'
   # derived from Railscasts #124: Beta Invites <http://railscasts.com/episodes/124-beta-invites>
   map.signup '/sup/:invite_token',  :controller => 'users',     :action => 'new'
-  
-  
   map.resources :invites, :as => 'inv'
   
-  map.resource :session
+  map.resources :users, :default => true, :has_many => :nodes do |users|
+    users.resources :piles
+  end
   
-  #map.user '/:id',                :controller => 'users',    :action => 'show',      :has_many => :nodes
-  map.resources :users, :has_many => :nodes
+  
+# back-end XHR URLs
   
   map.resources :nodes, :belongs_to => :user, :member => {
     :move_up => :put,
