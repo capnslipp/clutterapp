75a81d4fd04b534dad02475b785247d31884c463
Slippy Douglas <git@slippyd.com>
2009-09-24 17:57:52 -0700

    Added invite token to create_user in UsersControllerTest. NOW ALL (non-commmented-out) TESTS PASS! WEEEE!!!!

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -8,16 +8,16 @@ class UsersControllerTest < ActionController::TestCase
   # Be sure to include AuthenticatedTestHelper in test/test_helper.rb instead
   # Then, you can remove it from this and the units test.
   include AuthenticatedTestHelper
-
+  
   fixtures :users
-
+  
   def test_should_allow_signup
     assert_difference 'User.count' do
       create_user
       assert_response :redirect
     end
   end
-
+  
   def test_should_require_login_on_signup
     assert_no_difference 'User.count' do
       create_user(:login => nil)
@@ -25,7 +25,7 @@ class UsersControllerTest < ActionController::TestCase
       assert_response :success
     end
   end
-
+  
   def test_should_require_password_on_signup
     assert_no_difference 'User.count' do
       create_user(:password => nil)
@@ -33,7 +33,7 @@ class UsersControllerTest < ActionController::TestCase
       assert_response :success
     end
   end
-
+  
   def test_should_require_password_confirmation_on_signup
     assert_no_difference 'User.count' do
       create_user(:password_confirmation => nil)
@@ -41,7 +41,7 @@ class UsersControllerTest < ActionController::TestCase
       assert_response :success
     end
   end
-
+  
   def test_should_require_email_on_signup
     assert_no_difference 'User.count' do
       create_user(:email => nil)
@@ -50,12 +50,21 @@ class UsersControllerTest < ActionController::TestCase
     end
   end
   
-
   
-
-  protected
-    def create_user(options = {})
-      post :create, :user => { :login => 'quire', :email => 'quire@example.com',
-        :password => 'quire69', :password_confirmation => 'quire69' }.merge(options)
-    end
+  
+protected
+  
+  def create_user(options = {})
+    invite = Invite.new(:recipient_email => 'quire@example.com')
+    invite.save_with_validation(true)
+    
+    post :create, :user => {
+      :login => 'quire_',
+      :email => 'quire@example.com',
+      :password => 'quire69',
+      :password_confirmation => 'quire69',
+      :invite_token => invite.token
+    }.merge(options)
+  end
+  
 end
