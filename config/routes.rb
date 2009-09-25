56920bfb7cc88fa8f73f58569c33e9b1b210cdcd
Slippy Douglas <git@slippyd.com>
2009-09-24 20:09:51 -0700

    Un-nested resources again. It was breaking tests… and for good reason. Having to pass in the user every single time is dumb.

--- config/routes.rb
+++ config/routes.rb
@@ -15,19 +15,19 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :session
   
   #map.user '/:id',                :controller => 'users',    :action => 'show',      :has_many => :nodes
-  map.resources :users do |user|
-    user.resources :nodes, :member => {
-      :move_up => :put,
-      :move_down => :put,
-      :move_in => :put,
-      :move_out => :put,
-      :update_check_prop_checked => :put,
-      :set_note_prop_note => :put,
-      :set_priority_prop_priority => :put,
-      :set_tag_prop_tag => :put,
-      :set_text_prop_text => :put,
-      :set_time_prop_time => :put
-    }
-  end
+  map.resources :users, :has_many => :nodes
+  
+  map.resources :nodes, :belongs_to => :user, :member => {
+    :move_up => :put,
+    :move_down => :put,
+    :move_in => :put,
+    :move_out => :put,
+    :update_check_prop_checked => :put,
+    :set_note_prop_note => :put,
+    :set_priority_prop_priority => :put,
+    :set_tag_prop_tag => :put,
+    :set_text_prop_text => :put,
+    :set_time_prop_time => :put
+  }
   
 end
