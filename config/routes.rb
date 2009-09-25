0c54d83893bfe3f3415f6b200b808a74e00e199d
Slippy Douglas <git@slippyd.com>
2009-09-24 19:38:04 -0700

    Made routes actually nested. They weren't before.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ActionController::Routing::Routes.draw do |map|
   map.login   '/sin',               :controller => 'sessions',  :action => 'create'
   map.logout  '/sout',              :controller => 'sessions',  :action => 'destroy'
   map.register '/reg',              :controller => 'users',     :action => 'create'
-  #map.signup  '/signup',            :controller => 'users',     :action => 'new'
+  #map.signup  '/sup',            :controller => 'users',     :action => 'new'
   # derived from Railscasts #124: Beta Invites <http://railscasts.com/episodes/124-beta-invites>
   map.signup '/sup/:invite_token',  :controller => 'users',     :action => 'new'
   
@@ -14,19 +14,20 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resource :session
   
-  map.resources :users, :has_many => :nodes
   #map.user '/:id',                :controller => 'users',    :action => 'show',      :has_many => :nodes
+  map.resources :users do |user|
+    user.resources :nodes, :member => {
+      :move_up => :put,
+      :move_down => :put,
+      :move_in => :put,
+      :move_out => :put,
+      :update_check_prop_checked => :put,
+      :set_note_prop_note => :put,
+      :set_priority_prop_priority => :put,
+      :set_tag_prop_tag => :put,
+      :set_text_prop_text => :put,
+      :set_time_prop_time => :put
+    }
+  end
   
-  map.resources :nodes, :belongs_to => :user, :member => {
-    :move_up => :put,
-    :move_down => :put,
-    :move_in => :put,
-    :move_out => :put,
-    :update_check_prop_checked => :put,
-    :set_note_prop_note => :put,
-    :set_priority_prop_priority => :put,
-    :set_tag_prop_tag => :put,
-    :set_text_prop_text => :put,
-    :set_time_prop_time => :put
-  }
 end
