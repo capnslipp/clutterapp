a4fa9eee6b4e5469b526065f42a2f71f2bb85d71
Slippy Douglas <git@slippyd.com>
2009-09-27 10:40:28 -0700

    Fixed CheckNodeCell having no "new" or "edit" functionality.

--- app/cells/check_node_cell.rb
+++ app/cells/check_node_cell.rb
@@ -1,2 +1,14 @@
 class CheckNodeCell < NodeCell
+  
+  # check has no "new" or "edit" functionality
+  def new
+    show.merge(:view => 'show')
+  end
+  
+  
+protected
+  
+  def new_prop
+    CheckProp.new(:pile => @pile)
+  end
 end
