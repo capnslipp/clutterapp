e0170c0c737368a372062fe35ee2147628138fbb
Slippy Douglas <git@slippyd.com>
2009-09-27 22:47:14 -0700

    Wrote a performance test that pretty thoroughly tells us how slow rendering a lot of cells really is.

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -3,7 +3,29 @@ require 'performance_test_help'
 
 # Profiling results for each test method are written to tmp/performance.
 class BrowsingTest < ActionController::PerformanceTest
+  
   def test_homepage
     get '/'
   end
+  
+  def test_piles
+    bypass_authentication
+    
+    User.all.each do |u|
+      u.piles.each do |p|
+        p.root_node # create the root node first
+        10.times { p.nodes.rand.create_child!(:prop => Prop.rand) }
+        get "/#{u.to_param}/piles/#{p.to_param}"
+        assert_response :success
+      end
+    end
+  end
+  
+  
+protected
+  
+  def bypass_authentication
+    PilesController.any_instance.stubs(:logged_in? => true, :current_user => User.first)
+  end
+  
 end
