ade06cc6a58c76dcb516c3d80f64c7c9d7e1b13b
Slippy Douglas <git@slippyd.com>
2009-10-03 12:46:14 -0700

    Intermediate check-in: Tweaking main.css.erb to use Colorist (not fully installed yet, though).

--- app/helpers/stylesheets_helper.rb
+++ app/helpers/stylesheets_helper.rb
@@ -1,2 +1,6 @@
+require "#{RAILS_ROOT}/vendor/gems/colorist/lib/colorist/color"
+require "#{RAILS_ROOT}/vendor/gems/colorist/lib/colorist/core_extensions"
+
 module StylesheetsHelper
+  include Colorist
 end
