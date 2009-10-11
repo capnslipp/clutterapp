55bda21921b4bcf07a9123d2ecea8df7ea7db469
Slippy Douglas <git@slippyd.com>
2009-10-11 12:12:04 -0700

    Cleaned up error message.

--- app/models/node.rb
+++ app/models/node.rb
@@ -69,7 +69,7 @@ protected
   
   def validate
     #errors.add(:node, "either be root and have pile; or it must be neither root ") if root? ^ (pile.root_node != self) # causes stack overflow
-    errors.add(:prop, "must exist or #{self.class.to_s} must be root but not both") unless root? ^ prop
+    errors.add(:node, "must have a prop or be root, but not both") unless root? ^ prop
   end
   
   def increment_version
