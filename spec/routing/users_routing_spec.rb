6f08ce586084cde62cbc4a5b65d1dcf639f5f58c
Slippy Douglas <git@slippyd.com>
2009-10-14 23:52:13 -0700

    Fixed User routing specs.

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -2,32 +2,36 @@ require 'spec_helper'
 
 describe UsersController do
   describe "routing" do
-    it "recognizes and generates #index" do
-      { :get => "/users" }.should route_to(:controller => "users", :action => "index")
-    end
-
+    #it "recognizes and generates #index" do
+    #  { :get => "/users" }.should route_to(:controller => "users", :action => "index")
+    #end
+    
     it "recognizes and generates #new" do
-      { :get => "/users/new" }.should route_to(:controller => "users", :action => "new")
+      { :get => "/sup" }.should route_to(:controller => "users", :action => "new")
     end
-
+    
     it "recognizes and generates #show" do
-      { :get => "/users/1" }.should route_to(:controller => "users", :action => "show", :id => "1")
+      u = User.find(1)
+      { :get => "/#{u.to_param}" }.should route_to(:controller => "users", :action => "show", :id => u.to_param)
     end
-
+    
     it "recognizes and generates #edit" do
-      { :get => "/users/1/edit" }.should route_to(:controller => "users", :action => "edit", :id => "1")
+      u = User.find(1)
+      { :get => "/#{u.to_param}/edit" }.should route_to(:controller => "users", :action => "edit", :id => u.to_param)
     end
-
+    
     it "recognizes and generates #create" do
-      { :post => "/users" }.should route_to(:controller => "users", :action => "create") 
+      { :post => "/reg" }.should route_to(:controller => "users", :action => "create")
     end
-
+    
     it "recognizes and generates #update" do
-      { :put => "/users/1" }.should route_to(:controller => "users", :action => "update", :id => "1") 
+      u = User.find(1)
+      { :put => "/#{u.to_param}" }.should route_to(:controller => "users", :action => "update", :id => u.to_param)
     end
-
+    
     it "recognizes and generates #destroy" do
-      { :delete => "/users/1" }.should route_to(:controller => "users", :action => "destroy", :id => "1") 
+      u = User.find(1)
+      { :delete => "/#{u.to_param}" }.should route_to(:controller => "users", :action => "destroy", :id => u.to_param)
     end
   end
 end
