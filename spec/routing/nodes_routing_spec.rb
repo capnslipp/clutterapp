a81b21fe9e49667570ca18eae5352c6a349f11d9
Slippy Douglas <git@slippyd.com>
2009-10-20 01:34:08 -0700

    Made each spec test the named route helpers as well.

--- spec/routing/nodes_routing_spec.rb
+++ spec/routing/nodes_routing_spec.rb
@@ -4,31 +4,45 @@ describe NodesController do
   describe "routing" do
     
     it "recognizes and generates #index" do
-      { :get => "/test-user/piles/1/nodes" }.should route_to(:controller => 'nodes', :action => 'index', :user_id => 'test-user', :pile_id => '1')
+      expected_path = '/test-user/piles/1/nodes'
+      { :get => expected_path }.should route_to(:controller => 'nodes', :action => 'index', :user_id => 'test-user', :pile_id => '1')
+      user_pile_nodes_path(:pile_id => 1, :user_id => 'test-user').should == expected_path
     end
     
     it "recognizes and generates #new" do
-      { :get => "/test-user/piles/1/nodes/new" }.should route_to(:controller => 'nodes', :action => 'new', :user_id => 'test-user', :pile_id => '1')
+      expected_path = '/test-user/piles/1/nodes/new'
+      { :get => expected_path }.should route_to(:controller => 'nodes', :action => 'new', :user_id => 'test-user', :pile_id => '1')
+      new_user_pile_node_path(:pile_id => 1, :user_id => 'test-user').should == expected_path
     end
     
     it "recognizes and generates #show" do
-      { :get => "/test-user/piles/1/nodes/2" }.should route_to(:controller => 'nodes', :action => 'show', :user_id => 'test-user', :pile_id => '1', :id => '2')
+      expected_path = '/test-user/piles/1/nodes/2'
+      { :get => expected_path }.should route_to(:controller => 'nodes', :action => 'show', :user_id => 'test-user', :pile_id => '1', :id => '2')
+      user_pile_node_path(:id => 2, :pile_id => 1, :user_id => 'test-user').should == expected_path
     end
     
     it "recognizes and generates #edit" do
-      { :get => "/test-user/piles/1/nodes/2/edit" }.should route_to(:controller => 'nodes', :action => 'edit', :user_id => 'test-user', :pile_id => '1', :id => '2')
+      expected_path = '/test-user/piles/1/nodes/2/edit'
+      { :get => expected_path }.should route_to(:controller => 'nodes', :action => 'edit', :user_id => 'test-user', :pile_id => '1', :id => '2')
+      edit_user_pile_node_path(:id => 2, :pile_id => 1, :user_id => 'test-user').should == expected_path
     end
     
     it "recognizes and generates #create" do
-      { :post => "/test-user/piles/1/nodes" }.should route_to(:controller => 'nodes', :action => 'create', :user_id => 'test-user', :pile_id => '1')
+      expected_path = '/test-user/piles/1/nodes'
+      { :post => expected_path }.should route_to(:controller => 'nodes', :action => 'create', :user_id => 'test-user', :pile_id => '1')
+      user_pile_nodes_path(:pile_id => 1, :user_id => 'test-user').should == expected_path
     end
     
     it "recognizes and generates #update" do
-      { :put => "/test-user/piles/1/nodes/2" }.should route_to(:controller => 'nodes', :action => 'update', :user_id => 'test-user', :pile_id => '1', :id => '2')
+      expected_path = '/test-user/piles/1/nodes/2'
+      { :put => expected_path }.should route_to(:controller => 'nodes', :action => 'update', :user_id => 'test-user', :pile_id => '1', :id => '2')
+      user_pile_node_path(:id => 2, :pile_id => 1, :user_id => 'test-user').should == expected_path
     end
     
     it "recognizes and generates #destroy" do
-      { :delete => "/test-user/piles/1/nodes/2" }.should route_to(:controller => 'nodes', :action => 'destroy', :user_id => 'test-user', :pile_id => '1', :id => '2')
+      expected_path = '/test-user/piles/1/nodes/2'
+      { :delete => expected_path }.should route_to(:controller => 'nodes', :action => 'destroy', :user_id => 'test-user', :pile_id => '1', :id => '2')
+      user_pile_node_path(:id => 2, :pile_id => 1, :user_id => 'test-user').should == expected_path
     end
     
   end
