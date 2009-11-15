e5130f57972d1c013086941dc3856ea962c22987
Joshua Vera <josh@joshuavera.net>
2009-11-15 12:39:01 -0800

    Removed restful_auth methods from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,17 @@ require 'digest/sha1'
 
 class User < ActiveRecord::Base
   acts_as_authentic
-  # include Authentication
-  # include Authentication::ByPassword
-  # include Authentication::ByCookieToken
-  # 
+  
   validates_presence_of     :login
   validates_length_of       :login,    :within => 6..40
   validates_uniqueness_of   :login
-  validates_format_of       :login,    :with => Authentication.login_regex, :message => Authentication.bad_login_message
-  
-  validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
+
   validates_length_of       :name,     :maximum => 100
   
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
   validates_uniqueness_of   :email
-  validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
-  
-  
+
   
   # derived from Railscasts #124: Beta Invites <http://railscasts.com/episodes/124-beta-invites>
   
@@ -57,47 +50,6 @@ class User < ActiveRecord::Base
     user_list = find_all_by_login(login.to_s)
     user_list.first
   end
-  
-  
-  
-  # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
-  #
-  # uff.  this is really an authorization, not authentication routine.
-  # We really need a Dispatch Chain here or something.
-  # This will also let us return a human error message.
-  #
-  def self.authenticate(login, password)
-    return nil if login.blank? || password.blank?
-    u = find_by_login(login) # need to get the salt
-    u && u.authenticated?(password) ? u : nil
-  end
-  
-  
-  def login=(value)
-    write_attribute :login, (value ? value.downcase : nil)
-  end
-  
-  
-  def email=(value)
-    write_attribute :email, (value ? value.downcase : nil)
-  end
-  
-  
-  def invite_limit=(value)
-    write_attribute :invite_limit, (value.infinite? ? nil : value)
-  end
-  
-  
-  def has_name?
-    !attributes['name'].blank?
-  end
-  
-  
-  def name
-    return attributes['name'] unless attributes['name'].blank?
-    return attributes['login']
-  end
-  
   
   
   # derived from Railscasts #124: Beta Invites <http://railscasts.com/episodes/124-beta-invites>
