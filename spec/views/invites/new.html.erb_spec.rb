cf95b10c2bdc7c597e68a5770e11e88fb28fab2e
Slippy Douglas <git@slippyd.com>
2009-11-18 02:25:55 -0800

    Got invites/new view spec working by running activate_authlogic and then our custom login() spec helper.

--- spec/views/invites/new.html.erb_spec.rb
+++ spec/views/invites/new.html.erb_spec.rb
@@ -2,17 +2,18 @@ require 'spec_helper'
 
 describe "/invites/new.html.erb" do
   include InvitesHelper
-
+  
   before(:each) do
-    assigns[:invite] = stub_model(Invite,
-      :new_record? => true
-    )
+    assigns[:invite] = stub_model(Invite, :new_record? => true)
+    
+    activate_authlogic; login
   end
-
-  it "renders new invite form" #do
-  #  render
-  #
-  #  response.should have_tag("form[action=?][method=post]", invites_path) do
-  #  end
-  #end
+  
+  it "renders new invite form" do
+    render
+    
+    response.should have_tag("form[action=?][method=post]", invites_path) do
+    end
+  end
+  
 end
