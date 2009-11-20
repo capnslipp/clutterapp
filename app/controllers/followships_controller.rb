19670ec07822595aef17c6440d698fe2d036fb9b
Joshua Vera <josh@joshuavera.net>
2009-11-19 18:47:30 -0800

    Added toggle_follow method

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,8 +1,16 @@
 class FollowshipsController < ApplicationController
-  def create
-  end
+  before_filter :authorize
 
-  def destroy
+  def toggle_follow
+    @user = User.find_by_login(params[:login])
+    if @user.is_followee? current_user
+      flash[:notice] = "You are no longer following #{@user.login}"
+      current_user.unfollow(@user)
+    else
+      flash[:notice] - "You are now following #{@user.login}"
+      current_user.unfollow(@user)
+    end
+    redirect_to followship_path
   end
-
+  
 end
