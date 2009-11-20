3d9f113fc860cef2b6205756390918284d17d597
Joshua Vera <josh@joshuavera.net>
2009-11-19 17:23:10 -0800

    Added shares associations

--- app/models/pile.rb
+++ app/models/pile.rb
@@ -4,6 +4,10 @@ class Pile < ActiveRecord::Base
   
   has_many :nodes, :dependent => :destroy, :autosave => true
   
+  #Shares associations
+  has_many :private_shares
+  has_many :authorized_users, :through => :private_shares
+  
   has_one :pile_ref_prop, :as => :ref_pile
   
   #validates_presence_of   :root_node, :message => 'is required'
