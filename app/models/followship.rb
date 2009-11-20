3b3acf3f643804539234d60a20b3f1fc002daeaf
Joshua Vera <josh@joshuavera.net>
2009-11-19 18:48:06 -0800

    validates_uniqueness_of was screwing up User.follows method

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,7 +1,6 @@
 class Followship < ActiveRecord::Base
   belongs_to :user
   belongs_to :followee, :class_name => "User"
-  validates_uniqueness_of :followee_id, :scope => :user_id
   validates_presence_of :user_id, :followee_id
   
   def self.find_by_user_and_followee(user, followee)
