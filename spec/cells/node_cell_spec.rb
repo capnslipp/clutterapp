6cf81065b53322ba865ccb0e10da985fff36b3e5
Slippy Douglas <git@slippyd.com>
2009-11-27 17:30:26 -0800

    Fixed NodeCellSpecs by wrapping them all in a "describe NodeBodyCell" block.

--- spec/cells/node_cell_spec.rb
+++ spec/cells/node_cell_spec.rb
@@ -136,195 +136,202 @@ end
 
 
 =begin
-  NodeCell Variant Specs
+  NodeCell Variant (via NodeBodyCell) Specs
 =end
 
-describe CheckNodeCell do
-  it_should_behave_like "All NodeCells"
-  
-  before(:each) do
-    mock_node(
-      :prop => mock_model(CheckProp, :checked? => false, :badged? => CheckProp::badgeable?),
-      :children => @mocked_node_childre
-    )
-    @mock_node.prop.stubs(:class => CheckProp)
-  end
-  
-  describe("show action") { it_should_behave_like "Showing a NodeCell" }
-  
-  describe "form-based" do
-    describe("edit action") { it_should_behave_like "(NYI) Editing a NodeCell" }
-    describe("new action") { it_should_behave_like "(NYI) Newing a NodeCell" }
-  end
-  
-  after(:each) do
-    @result.should have_tag('input[type=checkbox]')
-  end
-end
 
-
-describe NoteNodeCell do
-  it_should_behave_like "All NodeCells"
+describe NodeBodyCell do
   
-  before(:each) do
-    mock_node(
-      :prop => mock_model(NoteProp, :note => 'A test note for >> you.', :badged? => NoteProp::badgeable?),
-      :children => @mocked_node_children
-    )
-    @mock_node.prop.stubs(:class => NoteProp)
-  end
   
-  describe("show action") { it_should_behave_like "Showing a NodeCell" }
-  
-  describe "form-based" do
-    describe("edit action") { it_should_behave_like "Editing a NodeCell" }
-    describe("new action") { it_should_behave_like "Newing a NodeCell" }
+  describe 'leading to', CheckNodeCell do
+    it_should_behave_like "All NodeCells"
     
-    after(:each) { @result.should have_tag('textarea') }
-  end
-  
-  after(:each) do
-    # @fix: Doesn't work via testing for some reason.
-    #@result.should include('A test note for &gt;&gt; you.') # make sure HTML escaping is being done
-  end
-end
-
-
-describe PileRefNodeCell do
-  it_should_behave_like "All NodeCells"
-  
-  before(:each) do
-    mock_node(
-      :prop => mock_model(PileRefProp, :ref_pile => mock_model(Pile), :badged? => PileRefProp::badgeable?),
-      :children => @mocked_node_children
-    )
-    @mock_node.prop.ref_pile.should_receive(:name).and_return("A Test Ref'd Pile & Stuff")
-    @mock_node.prop.ref_pile.stub(:owner).and_return(mock_model(User))
-    @mock_node.prop.ref_pile.should_receive(:root_node).at_least(:once).and_return(mock_model(Node))
-    @mock_node.prop.ref_pile.root_node.stub(:children).and_return([])
-    @mock_node.prop.stubs(:class => PileRefProp)
-  end
-  
-  describe("show action") { it_should_behave_like "Showing a NodeCell" }
-  
-  describe "form-based" do
-    describe("edit action") { it_should_behave_like "(NYI) Editing a NodeCell" }
-    describe("new action") { it_should_behave_like "(NYI) Newing a NodeCell" }
+    before(:each) do
+      mock_node(
+        :prop => mock_model(CheckProp, :checked? => false, :badged? => CheckProp::badgeable?),
+        :children => @mocked_node_childre
+      )
+      @mock_node.prop.stubs(:class => CheckProp)
+    end
     
-    #after(:each) {  @result.should have_tag('input[type=text]') }
-  end
-  
-  after(:each) do
-    #@result.should have_tag('section')
-    @result.should include("A Test Ref'd Pile &amp; Stuff") # make sure HTML escaping is being done
-  end
-end
-
-
-describe PriorityNodeCell do
-  it_should_behave_like "All NodeCells"
-  
-  before(:each) do
-    mock_node(
-      :prop => mock_model(PriorityProp, :priority => 110000000000, :badged? => PriorityProp::badgeable?),
-      :children => @mocked_node_children
-    )
-    @mock_node.prop.stubs(:class => PriorityProp)
-  end
-  
-  describe("show action") { it_should_behave_like "Showing a NodeCell" }
-  
-  describe "form-based" do
-    describe("edit action") { it_should_behave_like "Editing a NodeCell" }
-    describe("new action") { it_should_behave_like "Newing a NodeCell" }
+    describe("show action") { it_should_behave_like "Showing a NodeCell" }
     
-    after(:each) { @result.should have_tag('input[type=text]') }
-  end
-  
-  after(:each) do
-    # @fix: Doesn't work via testing for some reason.
-    #@result.should include('110000000000')
-  end
-end
-
-
-describe TagNodeCell do
-  it_should_behave_like "All NodeCells"
-  
-  before(:each) do
-    mock_node(
-      :prop => mock_model(TagProp, :tag => 'test-tag', :badged? => TagProp::badgeable?),
-      :children => @mocked_node_children
-    )
-    @mock_node.prop.stubs(:class => TagProp)
-  end
-  
-  describe("show action") { it_should_behave_like "Showing a NodeCell" }
-  
-  describe "form-based" do
-    describe("edit action") { it_should_behave_like "Editing a NodeCell" }
-    describe("new action") { it_should_behave_like "Newing a NodeCell" }
+    describe "form-based" do
+      describe("edit action") { it_should_behave_like "(NYI) Editing a NodeCell" }
+      describe("new action") { it_should_behave_like "(NYI) Newing a NodeCell" }
+    end
     
-    after(:each) { @result.should have_tag('input[type=text]') }
+    after(:each) do
+      @result.should have_tag('input[type=checkbox]')
+    end
   end
   
-  after(:each) do
-    # @fix: Doesn't work via testing for some reason.
-    #@result.should include('test-tag')
-  end
-end
-
-
-describe TextNodeCell do
-  it_should_behave_like "All NodeCells"
   
-  before(:each) do
-    mock_node(
-      :prop => mock_model(TextProp, :text => 'test text', :badged? => TextProp::badgeable?),
-      :children => @mocked_node_children
-    )
-    @mock_node.prop.stubs(:class => TextProp)
-  end
-  
-  describe("show action") { it_should_behave_like "Showing a NodeCell" }
-  
-  describe "form-based" do
-    describe("edit action") { it_should_behave_like "Editing a NodeCell" }
-    describe("new action") { it_should_behave_like "Newing a NodeCell" }
+  describe 'leading to', NoteNodeCell do
+    it_should_behave_like "All NodeCells"
     
-    after(:each) { @result.should have_tag('input[type=text]') }
-  end
-  
-  after(:each) do
-    # @fix: Doesn't work via testing for some reason.
-    #@result.should include('test text')
-  end
-end
-
-
-describe TimeNodeCell do
-  it_should_behave_like "All NodeCells"
-  
-  before(:each) do
-    @time_now = Time.now
-    mock_node(
-      :prop => mock_model(TimeProp, :time => @time_now, :badged? => TimeProp::badgeable?),
-      :children => @mocked_node_children
-    )
-    @mock_node.prop.stubs(:class => TimeProp)
-  end
-  
-  describe("show action") { it_should_behave_like "Showing a NodeCell" }
-  
-  describe "form-based" do
-    describe("edit action") { it_should_behave_like "Editing a NodeCell" }
-    describe("new action") { it_should_behave_like "Newing a NodeCell" }
+    before(:each) do
+      mock_node(
+        :prop => mock_model(NoteProp, :note => 'A test note for >> you.', :badged? => NoteProp::badgeable?),
+        :children => @mocked_node_children
+      )
+      @mock_node.prop.stubs(:class => NoteProp)
+    end
     
-    after(:each) { @result.should have_tag('input[type=text]') }
+    describe("show action") { it_should_behave_like "Showing a NodeCell" }
+    
+    describe "form-based" do
+      describe("edit action") { it_should_behave_like "Editing a NodeCell" }
+      describe("new action") { it_should_behave_like "Newing a NodeCell" }
+      
+      after(:each) { @result.should have_tag('textarea') }
+    end
+    
+    after(:each) do
+      # @fix: Doesn't work via testing for some reason.
+      #@result.should include('A test note for &gt;&gt; you.') # make sure HTML escaping is being done
+    end
   end
   
-  after(:each) do
-    # @fix: Doesn't work via testing for some reason.
-    #@result.should include(@time_now.to_s)
+  
+  describe 'leading to', PileRefNodeCell do
+    it_should_behave_like "All NodeCells"
+    
+    before(:each) do
+      mock_node(
+        :prop => mock_model(PileRefProp, :ref_pile => mock_model(Pile), :badged? => PileRefProp::badgeable?),
+        :children => @mocked_node_children
+      )
+      @mock_node.prop.ref_pile.should_receive(:name).and_return("A Test Ref'd Pile & Stuff")
+      @mock_node.prop.ref_pile.stub(:owner).and_return(mock_model(User))
+      @mock_node.prop.ref_pile.should_receive(:root_node).at_least(:once).and_return(mock_model(Node))
+      @mock_node.prop.ref_pile.root_node.stub(:children).and_return([])
+      @mock_node.prop.stubs(:class => PileRefProp)
+    end
+    
+    describe("show action") { it_should_behave_like "Showing a NodeCell" }
+    
+    describe "form-based" do
+      describe("edit action") { it_should_behave_like "(NYI) Editing a NodeCell" }
+      describe("new action") { it_should_behave_like "(NYI) Newing a NodeCell" }
+      
+      #after(:each) {  @result.should have_tag('input[type=text]') }
+    end
+    
+    after(:each) do
+      #@result.should have_tag('section')
+      @result.should include("A Test Ref'd Pile &amp; Stuff") # make sure HTML escaping is being done
+    end
   end
+  
+  
+  describe 'leading to', PriorityNodeCell do
+    it_should_behave_like "All NodeCells"
+    
+    before(:each) do
+      mock_node(
+        :prop => mock_model(PriorityProp, :priority => 110000000000, :badged? => PriorityProp::badgeable?),
+        :children => @mocked_node_children
+      )
+      @mock_node.prop.stubs(:class => PriorityProp)
+    end
+    
+    describe("show action") { it_should_behave_like "Showing a NodeCell" }
+    
+    describe "form-based" do
+      describe("edit action") { it_should_behave_like "Editing a NodeCell" }
+      describe("new action") { it_should_behave_like "Newing a NodeCell" }
+      
+      after(:each) { @result.should have_tag('input[type=text]') }
+    end
+    
+    after(:each) do
+      # @fix: Doesn't work via testing for some reason.
+      #@result.should include('110000000000')
+    end
+  end
+  
+  
+  describe 'leading to', TagNodeCell do
+    it_should_behave_like "All NodeCells"
+    
+    before(:each) do
+      mock_node(
+        :prop => mock_model(TagProp, :tag => 'test-tag', :badged? => TagProp::badgeable?),
+        :children => @mocked_node_children
+      )
+      @mock_node.prop.stubs(:class => TagProp)
+    end
+    
+    describe("show action") { it_should_behave_like "Showing a NodeCell" }
+    
+    describe "form-based" do
+      describe("edit action") { it_should_behave_like "Editing a NodeCell" }
+      describe("new action") { it_should_behave_like "Newing a NodeCell" }
+      
+      after(:each) { @result.should have_tag('input[type=text]') }
+    end
+    
+    after(:each) do
+      # @fix: Doesn't work via testing for some reason.
+      #@result.should include('test-tag')
+    end
+  end
+  
+  
+  describe 'leading to', TextNodeCell do
+    it_should_behave_like "All NodeCells"
+    
+    before(:each) do
+      mock_node(
+        :prop => mock_model(TextProp, :text => 'test text', :badged? => TextProp::badgeable?),
+        :children => @mocked_node_children
+      )
+      @mock_node.prop.stubs(:class => TextProp)
+    end
+    
+    describe("show action") { it_should_behave_like "Showing a NodeCell" }
+    
+    describe "form-based" do
+      describe("edit action") { it_should_behave_like "Editing a NodeCell" }
+      describe("new action") { it_should_behave_like "Newing a NodeCell" }
+      
+      after(:each) { @result.should have_tag('input[type=text]') }
+    end
+    
+    after(:each) do
+      # @fix: Doesn't work via testing for some reason.
+      #@result.should include('test text')
+    end
+  end
+  
+  
+  describe 'leading to', TimeNodeCell do
+    it_should_behave_like "All NodeCells"
+    
+    before(:each) do
+      @time_now = Time.now
+      mock_node(
+        :prop => mock_model(TimeProp, :time => @time_now, :badged? => TimeProp::badgeable?),
+        :children => @mocked_node_children
+      )
+      @mock_node.prop.stubs(:class => TimeProp)
+    end
+    
+    describe("show action") { it_should_behave_like "Showing a NodeCell" }
+    
+    describe "form-based" do
+      describe("edit action") { it_should_behave_like "Editing a NodeCell" }
+      describe("new action") { it_should_behave_like "Newing a NodeCell" }
+      
+      after(:each) { @result.should have_tag('input[type=text]') }
+    end
+    
+    after(:each) do
+      # @fix: Doesn't work via testing for some reason.
+      #@result.should include(@time_now.to_s)
+    end
+  end
+  
+  
 end
