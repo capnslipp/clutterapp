0ffe595b9df29bde7338aa9e1687ac8dd631d790
Slippy Douglas <git@slippyd.com>
2009-11-29 16:37:37 -0800

    Removing thoughbot-factory_girl and updated gem dependency for it.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,4 +28,4 @@ config.action_mailer.delivery_method = :test
 
 config.gem 'rspec', :lib => false, :version => '>= 1.2.9'
 config.gem 'rspec-rails', :lib => false, :version => '>= 1.2.9'
-config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :version => '>= 1.2.2', :source => 'http://gems.github.com'
+config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gemcutter.org'
