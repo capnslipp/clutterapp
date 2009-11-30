1e55d9f4520195e5716a903e33331daac081e1db
Slippy Douglas <git@slippyd.com>
2009-11-29 16:45:32 -0800

    Updating to Rails 2.3.5. Everything seems to still work.

--- config/environment.rb
+++ config/environment.rb
@@ -14,7 +14,7 @@ env_replace = {
 ENV['RAILS_ENV'] = env_replace[ENV['RAILS_ENV']] if env_replace.has_key?(ENV['RAILS_ENV'])
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
