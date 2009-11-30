46491d2cfd2362187b4566a82f4b94beb744d144
Slippy Douglas <git@slippyd.com>
2009-11-30 01:03:59 -0800

    Renamed sub-line to just line and used it a couple new place to make it looks better.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -570,7 +570,7 @@ function badgeRemove(link) {
 }
 
 $(function() {
-	$('.sub-line a.remove').live('click', function() { badgeRemove(this); return false; });
+	$('.line a.remove').live('click', function() { badgeRemove(this); return false; });
 });
 
 
