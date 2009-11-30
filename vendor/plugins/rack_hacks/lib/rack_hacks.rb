ac279929046e6edaeb7a9e6ee6e94628035eee75
Slippy Douglas <git@slippyd.com>
2009-11-29 16:34:05 -0800

    A couple comment for a hack a did a long time ago.

--- vendor/plugins/rack_hacks/lib/rack_hacks.rb
+++ vendor/plugins/rack_hacks/lib/rack_hacks.rb
@@ -3,11 +3,13 @@
 module Rack
   class Request
     def media_type
+      #content_type && content_type.split(/\s*[;,]\s*/, 2).first.downcase # original
       content_type && content_type.to_s.split(/\s*[;,]\s*/, 2).first.downcase
     end
     
     def media_type_params
       return {} if content_type.nil?
+      #content_type.split(/\s*[;,]\s*/)[1..-1]. # original
       content_type.to_s.split(/\s*[;,]\s*/)[1..-1].
         collect { |s| s.split('=', 2) }.
         inject({}) { |hash,(k,v)| hash[k.downcase] = v ; hash }
