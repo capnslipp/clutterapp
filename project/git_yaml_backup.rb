c880ead18da3256a69f22d34014eed481d1c2021
Slippy Douglas <git@slippyd.com>
2009-12-01 11:56:39 -0800

    Rough pass at back-up script. Just comments for now.

---
+++ project/git_yaml_backup.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+# rake db:data:dump
+
+# cp db/data.yml backup-dir/server-name/data.yml
+
+# mv db/data.yml db/data.2009-11-30.yml
+
+# cd backup-dir
+# git add server-name/data.yml
+# git commit -m 'automated database backup of server-name on 2009-11-30'
+# git push
