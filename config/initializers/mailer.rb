d7186c31d1d3a781819d865c00f2f74a52103a82
Slippy Douglas <git@slippyd.com>
2009-12-10 11:13:04 -0800

    Adjusted mailer settings to try to get password reset e-mails working.

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,11 +1,11 @@
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.default_charset = "utf-8"
+ActionMailer::Base.default_charset = 'utf-8'
 ActionMailer::Base.smtp_settings = {
-:enable_starttls_auto => true,
-:address => "smtp.gmail.com",
-:port => 587,
-:domain => "joshuavera.net",
-:authentication => :plain,
-:user_name => "do-not-reply@joshuavera.net",
-:password => "2825C9"
+  :enable_starttls_auto => true,
+  :address => 'localhost',
+  :port => 25,
+  :domain => 'clutterapp.com',
+  :user_name => 'knock-knock.whos-there.no-one.no-one-who@clutterapp.com',
+  #:password => '???',
+  :authentication => :plain,
 }
