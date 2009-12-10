66df7933ef3ddc87995ef516f0c8c8f23aab6e6c
Slippy Douglas <git@slippyd.com>
2009-12-10 11:35:59 -0800

    Added raise_delivery_errors config option to possibly help figure out what's going on.

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,5 +1,6 @@
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.default_charset = 'utf-8'
+ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.sendmail_settings = {
   :location       => '/usr/sbin/sendmail',
   :arguments      => '-i -t'
