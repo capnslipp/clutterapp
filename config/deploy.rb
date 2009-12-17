1058304e748a25fea3d72264d9e070ba7125b35b
Slippy Douglas <git@slippyd.com>
2009-12-17 13:24:54 -0800

    New deploy file with prod and stag server instances (pseduo-environments). Note: This now requires a deploy to be done as "cap prod deploy".

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,49 +1,54 @@
-#############################################################
-#	Application
-#############################################################
-
-
 set :application, 'clutterapp'
 
 
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/var/www/#{application}"
+# If you aren't deploying to /u/apps/#{application} on the target servers (which is the default), you can specify the actual location via the :deploy_to variable:
+#set :deploy_to, "/var/www/#{application}"
 
 
+task :prod do
+  set :rails_env, 'production'
+  
+  set :domain, 'prod.clutterapp.com'
+  role :web, domain                          # Your HTTP server, Apache/etc
+  role :app, domain                          # This may be the same as your `Web` server
+  role :db,  domain, :primary => true # This is where Rails migrations will run
+  #role :db,  'your slave db-server here'
+  
+  set :deploy_to, '/var/www/clutterapp'
+  
+  set :branch, 'master'
+end
 
-set :rails_env, 'staging'
-
-set :branch, 'master'
-
-
-set :domain, 'prod.orgclut.com'
-server domain, :app, :web
-role :db, domain, :primary => true
+task :stag do
+  set :rails_env, 'staging'
+  
+  set :domain, 'stag.clutterapp.com'
+  role :web, domain # Your HTTP server, Apache/etc
+  role :app, domain # This may be the same as your `Web` server
+  role :db,  domain, :primary => true#'174.143.208.40', :primary => true # This is where Rails migrations will run
+  #role :db,  "your slave db-server here"
+  
+  set :deploy_to, '/var/www/clutterapp-stag'
+  
+  set :branch, 'master'
+end
 
 set :user, 'rails' # SSH username, optional if the same as the dev computer username
 set :use_sudo, false
 
+
 set :repository,  'git@snaotn.6bitt.com:orgclut2.git'
 set :scm, :git
-#set :scm_username, "rails"
-#ssh_options[:forward_agent] = true # needed?
 set :deploy_via, :remote_cache
-
 set :git_enable_submodules, 1
 
-
-#set :scm_username, "rails"
-#set :scm_password, "_fill_in_"
-
-#set :checkout, "export" # stops Capistrano from copying SVN-specific files to the app
+#ssh_options[:forward_agent] = true # needed?
 
 
-#############################################################
-#	Passenger
-#############################################################
+
+=begin
+	Passenger
+=end
 
 namespace :passenger do
   desc "Restart Application"
@@ -62,9 +67,10 @@ namespace :deploy do
 end
 
 
-#############################################################
-#	DB Backup
-#############################################################
+
+=begin
+  DB Backup
+=end
 
 #desc "Update the shared backups directory."
 #task :symlink_backups_dir, :roles => :app do
