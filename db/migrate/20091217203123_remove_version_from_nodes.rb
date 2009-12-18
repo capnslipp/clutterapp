1d1d38e3fb831f1c3f398b251d950ff69eec76b1
Slippy Douglas <git@slippyd.com>
2009-12-18 11:46:38 -0800

    Reverting the DB changes from "Removing old cruft from past caching systems and attempts:" (commit 60a40ae18d8f810ac7a6ab7950799e4e3adff897) so that this can be safely testing with the existing production data.
    Files reverted:
    • db/migrate/20091217203123_remove_version_from_nodes.rb
    • db/schema.rb

--- db/migrate/20091217203123_remove_version_from_nodes.rb
+++
@@ -1,9 +0,0 @@
-class RemoveVersionFromNodes < ActiveRecord::Migration
-  def self.up
-    remove_column :nodes, :version
-  end
-  
-  def self.down
-    add_column :nodes, :version, :integer, :default => 0
-  end
-end
