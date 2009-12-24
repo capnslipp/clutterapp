168c840a68723c928677e9075bf96816b637ff8c
Slippy Douglas <git@slippyd.com>
2009-12-24 14:12:05 -0800

    Making prop required on Nodes; migration also clears out old nodes that don't have props.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091202012313) do
+ActiveRecord::Schema.define(:version => 20091224214713) do
 
   create_table "check_props", :force => true do |t|
     t.boolean  "checked",    :default => false, :null => false
@@ -39,10 +39,10 @@ ActiveRecord::Schema.define(:version => 20091202012313) do
     t.integer  "rgt"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "prop_id"
-    t.string   "prop_type"
-    t.integer  "version",    :default => 0
+    t.integer  "prop_id",                   :null => false
+    t.integer  "prop_type",                 :null => false
     t.integer  "pile_id",                   :null => false
+    t.integer  "version",    :default => 0
   end
 
   create_table "note_props", :force => true do |t|
