b8b7b3e75ec1aa440ac4b66f6423ebd462df203b
Slippy Douglas <git@slippyd.com>
2009-12-31 18:24:58 -0800

    Moved named_scope declarations from ItemNode to Node.

--- app/models/item_node.rb
+++ app/models/item_node.rb
@@ -7,23 +7,6 @@ class ItemNode < Node
   accepts_nested_attributes_for :prop, :allow_destroy => true
   
   
-  named_scope :varianted, lambda {|variant_or_name|
-    { :conditions => {:prop_type => Prop.derive_variant(variant_or_name).to_s} }
-  }
-  
-  named_scope :badgeable,     :conditions => {:prop_type => Prop.badgeable_variants.collect(&:to_s)}
-  named_scope :non_badgeable, :conditions => {:prop_type => Prop.non_badgeable_variants.collect(&:to_s)}
-  
-  named_scope :stackable,     :conditions => {:prop_type => Prop.stackable_variants.collect(&:to_s)}
-  named_scope :non_stackable, :conditions => {:prop_type => Prop.non_stackable_variants.collect(&:to_s)}
-  
-  named_scope :nodeable,      :conditions => {:prop_type => Prop.nodeable_variants.collect(&:to_s)}
-  named_scope :non_nodeable,  :conditions => {:prop_type => Prop.non_nodeable_variants.collect(&:to_s)}
-  
-  named_scope :deepable,      :conditions => {:prop_type => Prop.deepable_variants.collect(&:to_s)}
-  named_scope :non_deepable,  :conditions => {:prop_type => Prop.non_deepable_variants.collect(&:to_s)}
-  
-  
   def variant
     self.prop.variant
   end
