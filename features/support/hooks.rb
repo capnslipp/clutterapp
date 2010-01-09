dcef4696306703f747481cf72b4e2034ebdfbadd
Slippy Douglas <git@slippyd.com>
2010-01-09 10:06:36 -0800

    Added a $ global so that Dataset works with Cucumber.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,6 @@
 
 require 'dataset'
-
+$__cucumber_toplevel = self # to give Dataset access to Before()
 Cucumber::Rails::World.class_eval do
   include Dataset
   datasets_directory "#{RAILS_ROOT}/test/datasets"
