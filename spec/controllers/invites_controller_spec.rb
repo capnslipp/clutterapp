297c19222310280da5b02f723a9a0523ad811e9e
Slippy Douglas <git@slippyd.com>
2010-01-25 18:11:54 -0800

    Converted InvitesController tests to specs.

---
+++ spec/controllers/invites_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe InvitesController do
+  include InvitesHelper
+  
+  
+  it "should new" do
+    get :new
+    assert_template 'new'
+  end
+  
+  
+  describe "create" do
+    
+    it "should not when invalid" do
+      post :create
+      response.should redirect_to(root_url)
+    end
+    
+    
+    it "should not when blank invite email" do
+      post :create, { :invite => {:recipient_email => ''} }
+      response.should redirect_to(root_url)
+    end
+    
+    
+    it "should not when nil invite email" do
+      post :create, { :invite => {:recipient_email => nil} }
+      response.should redirect_to(root_url)
+    end
+    
+    
+    it "should when valid" do
+      post :create, { :invite => {:recipient_email => 'InvitesControllerTest@example.com'} }
+      response.should redirect_to(root_url)
+    end
+    
+  end
+  
+  
+end
