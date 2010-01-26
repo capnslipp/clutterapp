1bcd1f57743049db7c6097483d0987dda702ae18
Slippy Douglas <git@slippyd.com>
2010-01-25 18:13:01 -0800

    Removed old tests that are not longer needed.

--- test/functional/stylesheets_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class StylesheetsControllerTest < ActionController::TestCase
-end
--- test/unit/helpers/piles_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PilesHelperTest < ActionView::TestCase
-end
