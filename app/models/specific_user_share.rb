45ee3684570d27d34fd4d76057d096b3c6c26e49
Slippy Douglas <git@slippyd.com>
2010-03-13 16:58:41 -0800

    Fixed validations.

--- app/models/specific_user_share.rb
+++ app/models/specific_user_share.rb
@@ -1,6 +1,9 @@
 class SpecificUserShare < Share
   belongs_to :sharee, :class_name => User.name
   validates_presence_of :sharee
-  validates_uniqueness_of :sharee, :scope => 'pile_id' # share each pile with each "sharee" User at most once
+  
+  # share each pile with each "sharee" User at most once
+  validates_uniqueness_of :sharee_id, :scope => :pile_id
+  validates_uniqueness_of :pile_id, :scope => :sharee_id
   
 end
