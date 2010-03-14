98d521fedbcc71ed2c0ee111320280b1a34a20f9
Slippy Douglas <git@slippyd.com>
2010-03-13 17:49:09 -0800

    Now validating SpecificUserShare such that a Pile isn't shared with its owner.

--- app/models/specific_user_share.rb
+++ app/models/specific_user_share.rb
@@ -1,9 +1,16 @@
 class SpecificUserShare < Share
   belongs_to :sharee, :class_name => User.name
   validates_presence_of :sharee
+  validate :sharee_is_not_pile_owner
   
   # share each pile with each "sharee" User at most once
   validates_uniqueness_of :sharee_id, :scope => :pile_id
   validates_uniqueness_of :pile_id, :scope => :sharee_id
+  
+  protected
+  
+  def sharee_is_not_pile_owner
+    errors.add_to_base("sharing a Pile with its owner is pointless and not allowed") unless pile && pile.owner != sharee
+  end
   
 end
