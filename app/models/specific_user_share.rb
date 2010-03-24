3d1c35480112a3c7564e4d6d9d812bf12583db43
Slippy Douglas <git@slippyd.com>
2010-03-23 20:57:59 -0700

    Full edit functionality; needs update, new, create, and add functionality.

--- app/models/specific_user_share.rb
+++ app/models/specific_user_share.rb
@@ -13,4 +13,12 @@ class SpecificUserShare < Share
     errors.add_to_base("sharing a Pile with its owner is pointless and not allowed") unless pile && pile.owner != sharee
   end
   
+  def sharee_by_login
+    sharee.login
+  end
+  
+  def sharee_by_login=(login)
+    sharee = User.find_by_login(login)
+  end
+  
 end
