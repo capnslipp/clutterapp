fcbc051669a4230a883762c50b4199cb1c84a66c
Slippy Douglas <git@slippyd.com>
2010-03-30 20:22:25 -0700

    WIP: Eliminating all user-state-based redirects in favor of helpful message pages. (FogBugz #18) • Created "error" layout and "not_found" view for displaying the flash messages. • Fixed styling so that the flash messages display nicely both inside and outside of the top-bar. • Improved styling of “h-tags” and added styling for “sub-h” divs. • ApplicationController: added render_not_found method, cleaned up active_… methods, and switched have_… methods to render instead of redirecting.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   # include AuthenticatedSystem
   
   helper :all # include all helpers, all the time
-  helper_method :current_user_session, :current_user
+  helper_method :current_user_session, :current_user_session?, :current_user, :current_user?, :active_owner, :active_owner?, :active_pile, :active_pile?
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '9dec88372c3d408675df64aed646fffc'
@@ -15,27 +15,6 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   filter_parameter_logging :password, :password_confirmation
-  
-  
-  def active_owner_id
-    active_owner_id = params[:user_id]
-    active_owner_id ||= params[:id] if controller_name == 'users'
-    return active_owner_id
-  end
-  
-  def active_owner
-    return @active_owner ||= User.find_by_login(active_owner_id)
-  end
-  
-  def active_pile_id
-    active_pile_id = params[:pile_id]
-    active_pile_id ||= params[:id] if controller_name == 'piles'
-    return active_pile_id
-  end
-  
-  def active_pile
-    return @active_pile ||= active_owner.piles.find_by_id(active_pile_id)
-  end
   
   
 protected
@@ -68,11 +47,15 @@ protected
   
   def have_owner
     if active_owner_id.nil?
-      flash[:error] = "No user specified."
-      redirect_to current_user || home_url
+      flash.now[:error] = "No user specified."
+      render_not_found
+      return false
+      
     elsif active_owner.nil?
-      flash[:error] = "User “#{active_owner_id}” doesn't exist."
-      redirect_to current_user || home_url
+      flash.now[:error] = "User “#{active_owner_id}” doesn't exist."
+      render_not_found
+      return false
+      
     else
       return # success
     end
@@ -80,11 +63,15 @@ protected
   
   def have_pile
     if active_pile_id.nil?
-      flash[:error] = "No pile specified."
-      redirect_to [active_owner, :piles]
+      flash.now[:error] = "No pile specified."
+      render_not_found
+      return false
+      
     elsif active_pile.nil?
-      flash[:error] = "Pile “#{active_pile_id}” doesn't exist."
-      redirect_to [active_owner, :piles]
+      flash.now[:error] = "Pile “#{active_pile_id}” doesn't exist or doesn't belong to “#{active_owner_id}”."
+      render_not_found
+      return false
+      
     else
       return # success
     end
@@ -131,6 +118,37 @@ private
     !!current_user
   end
   
+  
+  def active_owner_id
+    active_owner_id = params[:user_id]
+    active_owner_id ||= params[:id] if controller_name == 'users'
+    return active_owner_id
+  end
+  
+  def active_owner
+    return @active_owner ||= User.find_by_login(active_owner_id)
+  end
+  
+  def active_owner?
+    !!active_owner
+  end
+  
+  def active_pile_id
+    active_pile_id = params[:pile_id]
+    active_pile_id ||= params[:id] if controller_name == 'piles'
+    return active_pile_id
+  end
+  
+  def active_pile
+    return nil unless active_owner?
+    return @active_pile ||= active_owner.piles.find_by_id(active_pile_id)
+  end
+  
+  def active_pile?
+    !!active_pile
+  end
+  
+  
   def require_user
     unless current_user
       store_location
@@ -153,6 +171,10 @@ private
   def redirect_back_or_default(default)
     redirect_to(session[:return_to] || default)
     session[:return_to] = nil
+  end
+  
+  def render_not_found
+    render :template => 'error/not_found', :layout => 'error', :status => :not_found
   end
   
 end
