0e3fd29fe5224b4dd021a5ab555565f3e5cf7419
Slippy Douglas <git@slippyd.com>
2010-04-02 20:29:26 -0700

    Adding ancestry column to nodes table.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100325040526) do
+ActiveRecord::Schema.define(:version => 20100403032712) do
 
   create_table "check_props", :force => true do |t|
     t.boolean  "checked",    :default => false, :null => false
@@ -36,7 +36,10 @@ ActiveRecord::Schema.define(:version => 20100325040526) do
     t.string   "prop_type"
     t.integer  "version",    :default => 0
     t.integer  "pile_id"
+    t.string   "ancestry"
   end
+
+  add_index "nodes", ["ancestry"], :name => "index_nodes_on_ancestry"
 
   create_table "note_props", :force => true do |t|
     t.text     "note",       :null => false
