8921b312357305c85027c307f2f5f1cd4f523297
Slippy Douglas <git@slippyd.com>
2010-04-17 12:58:17 -0700

    Experiment in making IE XPath-compatible and adding handier XPath utilities to Prototype.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,13 @@
 // ClutterApp main application JS
 
 CA = {};
+
+$x = function(expression) {
+	return document._getElementsByXPath(expression);
+}
+
+Element.addMethods({
+	x: function(element, expression) {
+		return document._getElementsByXPath(expression, element);
+	}
+});
