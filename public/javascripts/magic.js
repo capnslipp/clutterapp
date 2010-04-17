703548a9a3144a1f038b49204a34b6c4ef4be8c0
Slippy Douglas <git@slippyd.com>
2010-04-17 00:06:28 -0700

    Proper JS “CA.Item” class; hiding private vars within closures and grouping accessors together.

--- public/javascripts/magic.js
+++ public/javascripts/magic.js
@@ -1,55 +1,140 @@
-CA._activeItem = null;
-CA.getActiveItem = function() { return _activeItem; };
-CA.setActiveItem = function(item) {
-	if (item == this._activeItem)
-		return;
+/// call CA.Item() with the same args you'd given to $() in order to get an item element
+/// CA.Item() will spit back a CA.Item object if you passed in an item; othersie, nothing
+CA.Item = function(ref) {
+	var ref = $(ref);
 	
-	if (this._activeItem) {
-		this.inactiveItemBodyMorph( this._activeItem.select('> .cont > .body').first() );
-		
-		var textPropInput = this._activeItem.select('> .cont > .body > .cont > input.text.prop').first();
-		if (textPropInput) {
-			CA.activeItemInputChanged();
+	if (ref) {
+		// if a CA.Item object already exists for this element, return it
+		if (ref.caItem) {
+			return ref.caItem;
+		}
+		// otherwise, create and hook up a new CA.Item object
+		else if (ref.hasClassName('item')) {
+			var item = new CA.Item;
 			
-			textPropInput.remove();
-			var textProp = this._activeItem.select('> .cont > .body > .cont > div.text.prop').first();
-			textProp.setStyle({visibility: 'visible'});
+			initElement();
+			initBody();
+			initBack();
+			
+			return item;
 		}
 	}
 	
-	this._activeItem = $(item);
+	return undefined;
 	
-	if (this._activeItem) {
-		this.activeItemBodyMorph( this._activeItem.select('> .cont > .body').first() );
+	function initElement() {
+		// member private to the scope of the CA.Item instance
+		var _element = ref;
 		
-		var textProp = this._activeItem.select('> .cont > .body > .cont > div.text.prop').first();
-		if (textProp) {
-			var textContent = textProp.innerHTML.unescapeHTML().strip();
-			var textClass = textProp.readAttribute('class');
-			textProp.insert({
-				before: new Element('input', {
-					className: 'magic ' + textClass,
-					value: textContent
-				}),
-			});
-			var textPropInput = this._activeItem.select('> .cont > .body > .cont > input.text.prop').first();
-			var textPropDimens = textProp.getDimensions();
-			textPropInput.setStyle({
-				position: 'absolute',
-				width: textPropDimens.width + 'px',
-				height: textPropDimens.height + 'px',
-			});
-			textProp.setStyle({visibility: 'hidden'});
-			textPropInput.observe('keypress', function(e) {
-				CA.activeItemInputChanged();
-				//setTimeout('CA.activeItemInputChanged()', 500);
-			});
+		item.element = function() { return _element; }
+		
+		_element.caItem = item;
+	};
+	
+	function initBody() {
+		// member private to the scope of the CA.Item instance
+		var _body = null;
+		
+		item.body = function() {
+			if (!_body)
+				_body = this.element().select('> .cont > .body').first();
+			
+			return _body;
 		}
-	}
+	};
+	
+	function initBack() {
+		// member private to the scope of the CA.Item instance
+		var _back = null;
+		
+		item.back = function() {
+			if (!_back)
+				_back = this.body().select('> .back').first();
+			
+			return _back;
+		}
+	};
 };
-CA.activeItemInputChanged = function() {
-	var prop = this._activeItem.select('> .cont > .body > .cont > div.text.prop').first();
-	var input = this._activeItem.select('> .cont > .body > .cont > input.text.prop').first();
+
+var n = function() {
+	// member private to the scope of the CA.Item
+	var _activeItem = null;
+	
+	CA.Item.active = function() {
+		return _activeItem;
+	};
+	
+	CA.Item.prototype.activate = function() {
+		if (this.isActive())
+			return;
+		
+		if (_activeItem)
+			_activeItem.deactivate();
+		
+		_activeItem = this;
+		
+		this.visualActivate();
+	};
+	
+	CA.Item.prototype.deactivate = function() {
+		if (!this.isActive())
+			return;
+		
+		this.visualDeactivate();
+		
+		_activeItem = null;
+	};
+	
+	CA.Item.prototype.isActive = function() {
+		return this == _activeItem;
+	}
+}();
+
+CA.Item.prototype.visualActivate = function() {
+	this.bodyMorphActive();
+	
+	var textProp = this.body().select('> .cont > div.text.prop').first();
+	if (textProp) {
+		var textContent = textProp.innerHTML.unescapeHTML().strip();
+		var textClass = textProp.readAttribute('class');
+		textProp.insert({
+			before: new Element('input', {
+				className: 'magic ' + textClass,
+				value: textContent
+			}),
+		});
+		var textPropInput = this.body().select('> .cont > input.text.prop').first();
+		var textPropDimens = textProp.getDimensions();
+		textPropInput.setStyle({
+			position: 'absolute',
+			width: textPropDimens.width + 'px',
+			height: textPropDimens.height + 'px',
+		});
+		textProp.setStyle({visibility: 'hidden'});
+		
+		textPropInput.observe('keypress', function(e) {
+			this.inputChanged();
+		});
+	}
+}
+
+CA.Item.prototype.visualDeactivate = function() {
+	this.bodyMorphInactive();
+	
+	var textPropInput = this.body().select('> .cont > input.text.prop').first();
+	if (textPropInput) {
+		this.inputChanged();
+		
+		textPropInput.remove();
+		var textProp = this.body().select('> .cont > div.text.prop').first();
+		textProp.setStyle({visibility: 'visible'});
+	}
+}
+
+
+CA.Item.prototype.inputChanged = function() {
+	var prop = this.body().select('> .cont > div.text.prop').first();
+	var input = this.body().select('> .cont > input.text.prop').first();
 	
 	var textContent = input.getValue().escapeHTML();
 	prop.innerHTML = textContent;
@@ -60,16 +145,14 @@ CA.activeItemInputChanged = function() {
 		height: propDimens.height + 'px',
 	});
 }
-CA.activeItemBodyMorph = function(body) {
-	var back = body.select('> .back').first();
-	
-	body.addClassName('active');
-	back.setStyle({opacity: 1}); // in case it was hidden by inactiveItemBodyMorph
+CA.Item.prototype.bodyMorphActive = function(body) {
+	this.body().addClassName('active');
+	this.back().setStyle({opacity: 1}); // in case it was hidden by bodyMorphInactive
 }
-CA.inactiveItemBodyMorph = function(body) {
-	var back = body.select('> .back').first();
+CA.Item.prototype.bodyMorphInactive = function() {
+	var body = this.body();
 	
-	back.morph({opacity: 0}, {
+	this.back().morph({opacity: 0}, {
 		duration: 0.4,
 		transition: 'easeOutQuad',
 		after: function() {
@@ -79,9 +162,9 @@ CA.inactiveItemBodyMorph = function(body) {
 }
 
 document.observe('dom:loaded', function() {
-	$$('.item > .cont > .body').each(function(itemBody) {
-		itemBody.observe('click', function(event) {
-			CA.setActiveItem(itemBody.parentNode.parentNode);
-		})
+	$$('.item').each(function(item) {
+		item.select('> .cont > .body').first().observe('click', function(event) {
+			CA.Item(item).activate();
+		});
 	});
 });
